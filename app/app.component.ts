3cee9836d371026db1d789bce071faeb21e6be44
Manolo Carrasco <manolo@vaadin.com>
2016-04-01 10:42:49 +0200

    Step-5: Handling Ajax call to fill files array

--- app/app.component.ts
+++ app/app.component.ts
@@ -12,13 +12,27 @@ class UploadFile
   selector: 'my-app',
   templateUrl: 'app/app.component.html',
 })
-export class AppComponent  {
-  files: Array<UploadFile> = [{
-    progress: 20,
-    status: 'uploading',
-    name: 'foo'
-  }];
+export class AppComponent implements OnInit {
+  files: Array<UploadFile> = [];
 
   file: UploadFile;
+
+  upload: vaadin.VaadinUpload;
+
+  ajax: iron.IronAjax;
+
+  constructor(private elRef: ElementRef) {
+  }
+
+  ngOnInit() {
+    this.upload = this.elRef.nativeElement.querySelector('vaadin-upload');
+    this.ajax = this.elRef.nativeElement.querySelector('iron-ajax');
+    this.ajax.generateRequest();
+    (<HTMLElement><any>this.ajax).addEventListener("response", e => {
+      (<any>this.ajax.lastResponse).forEach(o => {
+        (<any>this.upload).push('files', o);
+      });
+    });
+  }
 
 }
